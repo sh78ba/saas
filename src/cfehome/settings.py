8d5736ac8c290b447b03b71673c6bfbe2835d85c
sh78ba <sh78ba@gmail.com>
2025-03-08 20:11:23 +0530

    asdf

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fg)#tp4kt77h(lci=3i$ssd2^vr(3sh5lau=x#1#(svfzf!9v&"
+SECRET_KEY = 'django-insecure-$+n#@h1w$n=syg37zc&gnf^y=dqbgm%z%*hai754n7@_a@pd3('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
