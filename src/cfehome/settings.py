11d260fea3bccfd99059a269c8c73e6dc488441c
sh78ba <sh78ba@gmail.com>
2025-03-08 20:17:04 +0530

    asdf

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -14,6 +14,10 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this is set
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
