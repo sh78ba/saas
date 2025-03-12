0172cf1d348e4d0f0c3c8b1d4f794d8a5b91789e
sh78ba <sh78ba@gmail.com>
2025-03-12 16:03:19 +0530

    test

--- src/saas/saas/views.py
+++ src/saas/saas/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "home.html", {"message": "Welcome to your Django App!"})
+    return render(request, "home.html", {"message": "Welcome to your Django App! Test completed"})
